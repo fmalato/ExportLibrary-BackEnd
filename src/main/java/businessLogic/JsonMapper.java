6709b6adfd5839af302e6bf80ec3afc579e7b7c2
francescogradi <francescogradi@gmail.com>
2020-10-05 16:02:07 +0200

    JsonMapper

--- src/main/java/businessLogic/JsonMapper.java
+++ src/main/java/businessLogic/JsonMapper.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import formModels.Form;
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class JsonMapper {
 
@@ -13,9 +16,24 @@ public class JsonMapper {
 
         ObjectMapper mapper = new ObjectMapper();
         try {
-            String json = mapper.writeValueAsString(form);
-            System.out.println("ResultingJSONstring = " + json);
-        } catch (JsonProcessingException e) {
+            String jsonString = mapper.writeValueAsString(form);
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(jsonString);
+
+            jsonObject.remove("id");
+            jsonObject.remove("name");
+            jsonObject.remove("category");
+
+            JSONObject jsonTmp = null;
+            for (Object key : jsonObject.keySet()) {
+                jsonTmp = new JSONObject();
+                jsonTmp.put("type", "text");
+                jsonTmp.put("label", key);
+                jsonTmp.put("value", null);
+                jsonForm.add(jsonTmp);
+            }
+
+        } catch (JsonProcessingException | ParseException e) {
             e.printStackTrace();
         }
 
